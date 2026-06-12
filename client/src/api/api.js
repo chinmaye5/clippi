acfc309772abdba07f54a84e9df15dff2609d6fc
Chinmaye HG <chinmaye115@gmail.com>
2026-06-12 21:19:24 +0530

    Remove hardcoded JSON content types

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -2,10 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL,
-    headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-    },
     withCredentials: true,
 });
 
